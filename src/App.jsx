47c3afd844fb5937e1fb19dd83a2ac36ac4e3b8c
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 13:39:31 +0200

    align travel jounal in center

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Header from "./components/Header";
 
 export default function App() {
   return (
-    <>
+    <div className="travel-journal">
       <Header></Header>
       <div className="cards-container">
         <Card></Card>
       </div>
-    </>
+    </div>
   );
 }
