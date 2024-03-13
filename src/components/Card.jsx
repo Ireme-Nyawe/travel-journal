58d9df8216c2f3384a951ff3cf20ced4f2feff3f
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 14:59:54 +0200

    add dynamic props to an element

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,26 @@
-export default function Card(props) {
+export default function Card({title,startDate,endDate,location,googlemap,deascription,image}) {
   return (
     <div className="card">
       <img
-        src="../src/assets/card-image.png"
+        src={image}
         alt="journal-image"
         className="card-image"
       />
       <div className="card-part">
         <div className="line1-card">
+          <div>
           <img
             src="../src/assets/location-icon.png"
             alt="location-icon"
             className="location-icon"
           />
-          <span>JAPAN</span>
-          <a href="">View on Google Maps</a>
+          <span>{location}</span>
+          </div>
+          <a href={googlemap}>View on Google Maps</a>
         </div>
-        <h2 className="title">{props.name}</h2>
-        <h4 className="date-range">1222-333</h4>
-        <p className="description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,380 feet). Mount Fuji is the single most popular tourist site in
-          Japan, for both Japanese and foreign tourists.
+        <h2 className="title">{title}</h2>
+        <h4 className="date-range">{startDate}-{endDate}</h4>
+        <p className="description">{deascription}
         </p>
       </div>
     </div>
