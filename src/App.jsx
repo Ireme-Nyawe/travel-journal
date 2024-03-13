c5fb540e4cc14f4210b349bafc4de9115c8c7b26
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 13:25:53 +0200

    setUp environment and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import Card from "./components/Card";
+import Header from "./components/Header";
+
 export default function App() {
-  return "TravelJournal";
+  return (
+    <>
+      <Header></Header>
+      <div className="cards-container">
+        <Card></Card>
+      </div>
+    </>
+  );
 }
---
+++ src/components/data.jsx
@@ -0,0 +1,30 @@
+export default [
+  {
+    title: "Mount Fuji",
+    location: "",
+    googlemap: "",
+    startDate: "",
+    endDate: "",
+    deascription:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    image: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+  {
+    title: "Sydney Opera House",
+    location: "",
+    googlemap: "",
+    startDate: "",
+    endDate: "",
+    deascription: "",
+    image: "https://source.unsplash.com/JmuyB_LibRo",
+  },
+  {
+    title: "Geirangerfjord",
+    location: "",
+    googlemap: "",
+    startDate: "",
+    endDate: "",
+    deascription: "",
+    image: "https://source.unsplash.com/3PeSjpLVtLg",
+  },
+];
