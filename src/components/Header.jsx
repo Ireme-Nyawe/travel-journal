9f6da47970851b49400460ee1637b92b2830d743
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 14:03:03 +0200

    design header-navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,8 @@
 export default function Header() {
-  return <nav className="nav">header</nav>;
+  return (
+    <nav className="nav">
+      <img src="../src/assets/world-icon.png" alt="world-icon" />
+      <h3>my travel journal.</h3>
+    </nav>
+  );
 }
